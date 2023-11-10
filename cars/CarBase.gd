0751eec1e3e2ed65bd41d1c2922c5f709b9485aa
Fredrik Asgard <fredrik.asgard@forse.no>
2023-11-10 17:34:52 +0100

    Basic car physics

--- cars/CarBase.gd
+++ cars/CarBase.gd
@@ -1,11 +1,65 @@
 extends CharacterBody2D
 
+# car properties
+var wheel_base = 70  # Distance from front to rear wheel
+var steering_angle = 15  # Amount that front wheel turns, in degrees
+var engine_power = 900  # Forward acceleration force.
+
+var braking = -450
+var max_speed_reverse = 250
+
+# environment / car properties
+var friction = -55
+var drag = -0.06
+
+var slip_speed = 400  # Speed where traction is reduced
+var traction_fast = 2.5 # High-speed traction
+var traction_slow = 10  # Low-speed traction
+
+# state
+var acceleration = Vector2.ZERO
+var steer_direction
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
+func _physics_process(delta):
+	acceleration = Vector2.ZERO
+	get_input()
+	apply_friction(delta)
+	calculate_steering(delta)
+	velocity += acceleration * delta
+	move_and_slide()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func apply_friction(delta):
+	if acceleration == Vector2.ZERO and velocity.length() < 50:
+		velocity = Vector2.ZERO
+	var friction_force = velocity * friction * delta
+	var drag_force = velocity * velocity.length() * drag * delta
+	acceleration += drag_force + friction_force
+
+func get_input():
+	var turn = Input.get_axis("steer_left", "steer_right")
+	steer_direction = turn * deg_to_rad(steering_angle)
+	if Input.is_action_pressed("accelerate"):
+		acceleration = transform.x * engine_power
+	if Input.is_action_pressed("brake"):
+		acceleration = transform.x * braking
+
+func calculate_steering(delta):
+	var rear_wheel = position - transform.x * wheel_base / 2.0
+	var front_wheel = position + transform.x * wheel_base / 2.0
+	rear_wheel += velocity * delta
+	front_wheel += velocity.rotated(steer_direction) * delta
+	var new_heading = (front_wheel - rear_wheel).normalized()
+	# choose which traction value to use - at lower speeds, slip should be low
+	var traction = traction_slow
+	if velocity.length() > slip_speed:
+		traction = traction_fast
+	var d = new_heading.dot(velocity.normalized())
+	if d > 0:
+		velocity = lerp(velocity, new_heading * velocity.length(), traction * delta)
+	if d < 0:
+		velocity = -new_heading * min(velocity.length(), max_speed_reverse)
+	rotation = new_heading.angle()

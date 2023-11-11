9afcb57fc4a26ab472b2cedf1c7e62c9eabc6c2e
Fredrik Asgard <fredrik.asgard@forse.no>
2023-11-11 12:03:42 +0100

    Small change to ammo pickup

--- powerups/ammo.gd
+++ powerups/ammo.gd
@@ -5,8 +5,8 @@ class_name Ammo
 func _ready():
 	var tween = get_tree().create_tween()
 	
-	tween.tween_property($Sprite2D, "scale", Vector2(0.9, 0.9), 0.3)
-	tween.tween_property($Sprite2D, "scale", Vector2(1.2, 1.2), 0.3)
+	tween.tween_property(self, "scale", Vector2(0.9, 0.9), 0.3)
+	tween.tween_property(self, "scale", Vector2(1.2, 1.2), 0.3)
 	tween.set_loops()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.

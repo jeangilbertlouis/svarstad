da5027b102063133c296f5452190b7f9a48080c3
Fredrik Asgard <fredrik.asgard@forse.no>
2023-11-11 12:01:45 +0100

    Adding speedometer in hud

--- hud/hud.gd
+++ hud/hud.gd
@@ -7,6 +7,10 @@ func _ready():
 	pass # Replace with function body.
 
 
+var speed_text = "%.0f km/h"
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if player != null:
+		var velocity = player.velocity.length() / 6
+		$MarginContainer2/car_box/speed_box/speed_label.text = speed_text % velocity

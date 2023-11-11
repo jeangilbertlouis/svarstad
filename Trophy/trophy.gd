3b094ed89c9d5b472d2938d8246a9d7e19238c7b
Kjetil Eide <kjetil.eide@telia.no>
2023-11-11 11:33:53 +0100

    trophy

--- Trophy/trophy.gd
+++ Trophy/trophy.gd
@@ -1,16 +1,11 @@
-extends Node2D
+extends Node
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	position
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
-
-
-func _on_ammo_body_entered(body):
-	print("Ammo")
